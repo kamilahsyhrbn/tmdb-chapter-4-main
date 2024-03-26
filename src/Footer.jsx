a54a1bd1a57b57e9e9bb48473a8f5b05a6d070a3
Kamilah Syahrabanu <kamilahsyahrabanu@gmail.com>
2024-03-26 17:04:02 +0700

    final commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className=" border-t-2  py-2 text-center text-white">
+    <div className="mt-6 py-2 text-center text-white">
       <div className="text-xs">&copy; Copyrigth Kelompok 4 </div>
     </div>
   );
